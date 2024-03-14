526dbd5577e03206054fa93d8ecc74fbf3895b92
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-03-14 17:33:43 +0100

    bootstrap

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Model Controller</title>
+
+    @vite(['resources/js/app.js'])
 </head>
 <body>
 
+    <h1>Ciao</h1>
     
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,7 +12,8 @@ export default defineConfig({
     //Aggiungiamo un alias per la cartella /resources/
     resolve: {
         alias: {
-            '~resources': '/resources/'
+            '~resources': '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
     },
 });
